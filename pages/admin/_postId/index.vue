5fe2aeb0c22995ef5eaae1a11acf8f7c88a1354e
utbroker <doug@dougevans.net>
2018-02-07 22:23:55 -0700

    Second Assignment Commit

---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="admin-post-page">
+      <section class="update-form">
+          <AdminPostForm :post="loadedPost"/>
+      </section>
+  </div>
+</template>
+
+<script>
+import AdminPostForm from '@/components/Admin/AdminPostForm'
+export default {
+  components: {
+      AdminPostForm
+  },
+  layout: 'admin',
+  data() {
+      return {
+          loadedPost: {
+              author: 'Doug',
+              title: 'My Awesome Post',
+              content: 'Thanks for that',
+              thumbnailLink: 'https://articles-images.sftcdn.net/wp-content/uploads/sites/3/2017/05/computerworld_tech_forecast_2017_hottest-tech-skills-for-2017-100692085-large.jpg'
+          }
+      }
+  }
+}
+</script>
+
+<style scoped>
+.update-form {
+  width: 90%;
+  margin: 20px auto;
+}
+
+@media (min-width: 768px) {
+  .update-form {
+    width: 500px;
+  }
+}
+</style>
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,10 +1,7 @@
 <template>
-<div class="home-page">
-  <section class="intro">
-    <h1>Get the latest tech news</h1>
-  </section>
-  <PostList :posts="loadedPosts"/>
-</div>
+  <div class="posts-page">
+    <PostList :posts="loadedPosts"/>
+  </div>
 </template>
 
 <script>
@@ -34,42 +31,15 @@ export default {
     })
     }, 1500)
   }
-  // data() {
-  //   return 
-  // }
 }
 </script>
 
-
 <style scoped>
-.intro {
-  height: 300px;
-  position: relative;
-  padding: 30px;
-  box-sizing: border-box;
-  background-image: url('~assets/images/main-page-background.jpg');
-  background-position: center;
-  background-size: cover;
+.posts-page {
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 
-.intro h1 {
-  position: absolute;
-  top: 10%;
-  left: 5%;
-  width: 90%;
-  font-size: 1.5rem;
-  color: black;
-  background-color: rgb(211, 211, 211);
-  padding: 10px;
-  border-radius: 10px;
-  box-shadow: 3px 3px 3px black;
-  box-sizing: border-box;
-  border: 1px solid black;
-}
-
-@media (min-width: 768px) {
-  .intro h1 {
-    font-size: 2rem;
-  }
-}
 </style>
+
